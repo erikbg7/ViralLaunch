5ddf345fa82e7ec518ef0bfbf2f8e7e010ca94a9
Erik <erik.bg7@gmail.com>
2025-02-26 12:19:02 +0100

    test product creation from api endpoint

--- src/routes/api/get-reddit-active-users/+server.ts
+++ src/routes/api/get-reddit-active-users/+server.ts
@@ -1,10 +1,14 @@
 import { SUPABASE_SERVICE_KEY } from '$env/static/private';
+import { createProduct } from '$lib/server/db/product.model.js';
 import { getAllSubreddits, insertHourlyAverage } from '$lib/server/db/subreddit.model.js';
 import { RedditAPI } from '$lib/server/reddit-api.service';
 import { json } from '@sveltejs/kit';
 
 export const POST = async (event) => {
 	console.log('[REDDIT API] Webhook called');
+
+	await createProduct('5ykowyszumtyuqlrmplweodv', 'kakatu');
+
 	const authHeader = event.request.headers.get('Authorization');
 	if (!authHeader || authHeader !== `Bearer ${SUPABASE_SERVICE_KEY}`) {
 		return json({ error: 'Unauthorized' }, { status: 401 });
