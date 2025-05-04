9606e1eab04304e47a5fbdd6f84ba7f518fed8c2
Erik <erik.bg7@gmail.com>
2025-05-04 23:19:14 +0200

    feat: get last week records repository function

--- src/lib/server/repositories/record.repository.ts
+++ src/lib/server/repositories/record.repository.ts
@@ -5,6 +5,36 @@ import { SubredditRepository } from '$lib/server/repositories/subreddit.reposito
 import { floorTo20Minutes } from '$lib/timezone';
 
 export class RecordRepository {
+	static async getLastWeekRecords(subredditId: string) {
+		const records = await db.execute<SubredditRecord>(sql`
+			WITH cutoff AS (
+  				SELECT now() AT TIME ZONE 'UTC' - INTERVAL '7 days' AS cutoff_ts
+			),
+			start_record AS (
+				SELECT
+					sr.timestamp AS start_ts
+				FROM
+					subreddit_record sr
+					, cutoff c
+				WHERE
+					sr.timestamp > c.cutoff_ts
+					AND sr.subreddit_id = ${subredditId}
+				ORDER BY
+					sr.timestamp
+				LIMIT 1
+			)
+			SELECT sr.* FROM subreddit_record sr
+			CROSS JOIN start_record s
+			WHERE
+				sr.timestamp >= s.start_ts
+				AND sr.subreddit_id = ${subredditId}
+			ORDER BY
+				sr.timestamp;
+		`);
+
+		return records;
+	}
+
 	static async getAllRecords(subredditId: string) {
 		const subreddit = await SubredditRepository.getById(subredditId);
 
